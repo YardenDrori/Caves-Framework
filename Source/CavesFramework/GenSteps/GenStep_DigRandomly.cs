fbd67903bb494da55b1d93544ca093269b6a9056
YardenDrori <yarden.drori.yd@gmail.com>
2026-08-15 11:35:51 +0300

    feat: add pre-processing gensteps for CA

---
+++ Source/CavesFramework/GenSteps/GenStep_DigRandomly.cs
@@ -0,0 +1,23 @@
+using Verse;
+
+namespace CavesFramework;
+
+public class GenStep_DigRandomly : GenStep
+{
+  public FloatRange digPercentage = new FloatRange(0.55f, 0.55f);
+
+  public override int SeedPart => 1506875925;
+
+  public override void Generate(Map map, GenStepParams parms)
+  {
+    float fillPercentageInstance = digPercentage.RandomInRange;
+    MapGenFloatGrid caves = MapGenerator.Caves;
+    foreach (IntVec3 allcell in map.AllCells)
+    {
+      if (Rand.Chance(fillPercentageInstance))
+      {
+        caves[allcell] = 1f;
+      }
+    }
+  }
+}
---
+++ Source/CavesFramework/GenSteps/GenStep_SetMapEdgesToRock.cs
@@ -0,0 +1,29 @@
+using Verse;
+
+namespace CavesFramework;
+
+public class GenStep_SetMapEdgesToRock : GenStep
+{
+  public FloatRange thickness = new FloatRange(1f, 1f);
+
+  public override int SeedPart => 952084420;
+
+  public override void Generate(Map map, GenStepParams parms)
+  {
+    float thicknessInstance = thickness.RandomInRange;
+    int guaranteedThickness = (int)thicknessInstance;
+    float additionalThicknessChance = thicknessInstance - guaranteedThickness;
+    MapGenFloatGrid caves = MapGenerator.Caves;
+    foreach (IntVec3 allcell in map.AllCells)
+    {
+      if (allcell.CloseToEdge(map, guaranteedThickness))
+      {
+        caves[allcell] = -1;
+      }
+      else if (allcell.CloseToEdge(map, guaranteedThickness + 1) && Rand.Chance(additionalThicknessChance))
+      {
+        caves[allcell] = -1;
+      }
+    }
+  }
+}

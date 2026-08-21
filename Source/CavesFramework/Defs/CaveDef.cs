a60e06392bab0fb33a118ce92f16e06d82fd6123
YardenDrori <yarden.drori.yd@gmail.com>
2026-08-21 08:29:21 +0300

    feat: add collapse props to cavedef

--- Source/CavesFramework/Defs/CaveDef.cs
+++ Source/CavesFramework/Defs/CaveDef.cs
@@ -6,38 +6,40 @@ namespace CavesFramework;
 
 public class CaveShapeEntry
 {
-    public CaveShapeDef shape;
-    public float shapeWeight = 1f;
+  public CaveShapeDef shape;
+  public float shapeWeight = 1f;
 }
 
 public class CaveBiomeEntry
 {
-    public BiomeDef biome;
-    public float biomeWeight = 1f;
+  public BiomeDef biome;
+  public float biomeWeight = 1f;
 }
 
 public class OptionalMutatorEntry
 {
-    public TileMutatorDef mutator;
-    public float mutatorWeight = 1f;
+  public TileMutatorDef mutator;
+  public float mutatorWeight = 1f;
 }
 
 public class OptionalMutators
 {
-    public List<OptionalMutatorEntry> optionalMutators = new();
+  public List<OptionalMutatorEntry> optionalMutators = new();
 
-    public int maxOptionalMutatorsActive = 1;
-    public float chanceForOptionalMutators = 0.15f;
-    public float additionalMutatorChanceMult = 0.5f;
+  public int maxOptionalMutatorsActive = 1;
+  public float chanceForOptionalMutators = 0.15f;
+  public float additionalMutatorChanceMult = 0.5f;
 }
 
 public class CaveDef : Def
 {
-    public List<CaveShapeEntry> shapes = new();
-    public List<CaveBiomeEntry> biomes = new();
+  public List<CaveShapeEntry> shapes = new();
+  public List<CaveBiomeEntry> biomes = new();
 
-    public List<TileMutatorDef> mutators = new();
-    public OptionalMutators optionalMutators;
+  public List<TileMutatorDef> mutators = new();
+  public OptionalMutators optionalMutators;
 
-    public bool disableShadows = true;
+  public CaveCollapseTimerProperties collapseTimerProps;
+
+  public bool disableShadows = true;
 }

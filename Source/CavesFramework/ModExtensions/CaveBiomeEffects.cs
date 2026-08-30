52d894dc04ba46f8e9e3fafb529e766bb5967dec
YardenDrori <yarden.drori.yd@gmail.com>
2026-08-30 13:35:16 +0300

    feat: overhaul the mod extension to allow configuration of spawn rates

--- Source/CavesFramework/ModExtensions/CaveBiomeEffects.cs
+++ Source/CavesFramework/ModExtensions/CaveBiomeEffects.cs
@@ -3,15 +3,52 @@ using Verse;
 
 namespace CavesFramework;
 
+public class EffecterSpawnerConfig
+{
+  public EffecterDef effecter;
+
+  //fixed rate, ignores how big the cave is
+  public float? mtbHoursPerSpawn;
+
+  //scales with cave size, higher -> spawn more
+  public float? spawnsPerHourPer10kEmptyCells;
+}
+
 public class CaveEffects : DefModExtension
 {
-  public List<EffecterDef> effectors;
+  public List<EffecterSpawnerConfig> effectsToSpawn;
 
   public override IEnumerable<string> ConfigErrors()
   {
-    if (effectors.NullOrEmpty())
+    if (effectsToSpawn.NullOrEmpty())
     {
       yield return "effects not specified.";
+    }
+    foreach (EffecterSpawnerConfig effect in effectsToSpawn)
+    {
+      //xor check
+      if (effect.mtbHoursPerSpawn.HasValue && effect.spawnsPerHourPer10kEmptyCells.HasValue)
+      {
+        yield return "an effect has both mtbHoursPerSpawn and spawnsPerHourPer10kEmptyCells populated, they are mutually exclusive.";
+      }
+      if (!effect.mtbHoursPerSpawn.HasValue && !effect.spawnsPerHourPer10kEmptyCells.HasValue)
+      {
+        yield return "an effect doesn't specify mtbHoursPerSpawn nor spawnsPerHourPer10kEmptyCells.";
+      }
+
+      //meaningfull input check
+      if (effect.mtbHoursPerSpawn.HasValue && effect.mtbHoursPerSpawn <= 0)
+      {
+        yield return "an effect's mtbHoursPerSpawn has a non positive value";
+      }
+      if (effect.spawnsPerHourPer10kEmptyCells.HasValue && effect.spawnsPerHourPer10kEmptyCells <= 0)
+      {
+        yield return "an effect's spawnsPerHourPer10kEmptyCells has a non positive value";
+      }
+      if (effect.effecter == null)
+      {
+        yield return "an effect has a null def.";
+      }
     }
   }
 }

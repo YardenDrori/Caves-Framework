976e64bf9a0f3a59d0047a70b0fe2df5e94d1fe9
YardenDrori <yarden.drori.yd@gmail.com>
2026-08-31 08:58:01 +0300

    refactor: move CaveEffectsConfig to its dedicated file for clearer intention to not be tied to biome's effects

--- Source/CavesFramework/ModExtensions/CaveBiomeEffects.cs
+++ Source/CavesFramework/ModExtensions/CaveBiomeEffects.cs
@@ -3,17 +3,6 @@ using Verse;
 
 namespace CavesFramework;
 
-public class EffecterSpawnerConfig
-{
-  public EffecterDef effecter;
-
-  //fixed rate, ignores how big the cave is
-  public float? mtbHoursPerSpawn;
-
-  //scales with cave size, higher -> spawn more
-  public float? spawnsPerHourPer10kEmptyCells;
-}
-
 public class CaveEffects : DefModExtension
 {
   public List<EffecterSpawnerConfig> effectsToSpawn;

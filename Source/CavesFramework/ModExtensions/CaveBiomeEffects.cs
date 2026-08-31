cf6f39134f11d736f28610c62e0664f5f6191bf2
YardenDrori <yarden.drori.yd@gmail.com>
2026-08-31 10:06:49 +0300

    refactor: move error handling to the effects class directly

--- Source/CavesFramework/ModExtensions/CaveBiomeEffects.cs
+++ Source/CavesFramework/ModExtensions/CaveBiomeEffects.cs
@@ -12,31 +12,13 @@ public class CaveEffects : DefModExtension
     if (effectsToSpawn.NullOrEmpty())
     {
       yield return "effects not specified.";
+      yield break;
     }
     foreach (EffecterSpawnerConfig effect in effectsToSpawn)
     {
-      //xor check
-      if (effect.mtbHoursPerSpawn.HasValue && effect.spawnsPerHourPer10kEmptyCells.HasValue)
+      foreach (string err in effect.ConfigErrors())
       {
-        yield return "an effect has both mtbHoursPerSpawn and spawnsPerHourPer10kEmptyCells populated, they are mutually exclusive.";
-      }
-      if (!effect.mtbHoursPerSpawn.HasValue && !effect.spawnsPerHourPer10kEmptyCells.HasValue)
-      {
-        yield return "an effect doesn't specify mtbHoursPerSpawn nor spawnsPerHourPer10kEmptyCells.";
-      }
-
-      //meaningfull input check
-      if (effect.mtbHoursPerSpawn.HasValue && effect.mtbHoursPerSpawn <= 0)
-      {
-        yield return "an effect's mtbHoursPerSpawn has a non positive value";
-      }
-      if (effect.spawnsPerHourPer10kEmptyCells.HasValue && effect.spawnsPerHourPer10kEmptyCells <= 0)
-      {
-        yield return "an effect's spawnsPerHourPer10kEmptyCells has a non positive value";
-      }
-      if (effect.effecter == null)
-      {
-        yield return "an effect has a null def.";
+        yield return err;
       }
     }
   }
